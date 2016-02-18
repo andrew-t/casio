378c0869d5f8ee953e18b2be108231e385284afe
Andrew Taylor <mac@andrewt.net>
2016-02-18 19:16:45 +0000

    small tweaks based on FACTS

--- casio.js
+++ casio.js
@@ -2,6 +2,7 @@ var digits,
 	next,
 	ticker,
 	score,
+	attempts,
 	faller = null;
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -25,6 +26,7 @@ function init() {
 	if (ticker)
 		clearInterval(ticker);
 	score = 0;
+	attempts = 0;
 	digits = [];
 	for (var i = 0; i < 10; ++i)
 		digits.push(new Digit());
@@ -41,27 +43,28 @@ function tick() {
 			--faller;
 		} else {
 			// Collision!
-			var isMatch = true,
-				fallingDigit = digits[faller];
+			var fallingDigit = digits[faller];
+			// The faller has landed. Destroy it.
 			digits[faller] = new Digit();
+			// Toggle all the bits that match the faller.
 			fallingDigit.segments.forEach(function(on, i) {
-				if (on && !digits[faller - 1].segments[i])
-					isMatch = false;
+				if (on)
+					digits[faller - 1].segments[i] =
+						!digits[faller - 1].segments[i];
 			});
-			if (isMatch) {
-				fallingDigit.segments.forEach(function(on, i) {
-					if (on)
-						digits[faller - 1].segments[i] = false;
-				});
-				if (digits[faller - 1].isBlank()) {
-					// Hooray! Do something?
-					++score;
-					if (faller == 1)
-						// We just destroyed the last digit, so add one now rather than waiting.
-						addNumber();
-				}
-			} else
-				addNumber();
+			// Have we cleared the digit?
+			if (digits[faller - 1].isBlank()) {
+				// Hooray! Do something?
+				++score;
+				attempts = 0;
+				if (faller == 1)
+					// We just destroyed the last digit, so add one now rather than waiting.
+					addNumber();
+			} else {
+				// You only get (TODO) 5 goes at each digit.
+				if (++attempts >= 5)
+					addNumber();
+			}
 			faller = null;
 		}
 	} else if (digits[digits.length - 1].isBlank()) {
@@ -74,9 +77,11 @@ function tick() {
 
 // Currently, adds an 8 after your last existing digit.
 function addNumber() {
+	attempts = 0;
 	for (var i = 0; i < 10; ++i)
 		if (digits[i].isBlank()) {
-			digits[i] = Digit.eight();
+			digits[i] = display.getDigit(Math.floor(
+				Math.random() * 10).toString());
 			draw();
 			return;
 		}
@@ -86,7 +91,7 @@ function addNumber() {
 function pickNext() {
 	next = new Digit();
 	var offset = (Math.random() < 0.5) ? 0 : 3,
-		toLight = (Math.random() < 0.5) ? 2 : 3;
+		toLight = -~(Math.random() * 3);
 	while (toLight) {
 		var n = ~~(Math.random() * 4) + offset;
 		if (!next.segments[n]) {
